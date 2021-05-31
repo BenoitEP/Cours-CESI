30b8148c24332e4329e6fbf59a47c67af2390425
BenoitEP <benoit.epiard@gmail.com>
2021-05-31 15:07:11 +0200

    Maj 31.05.21

---
+++ Circle Area/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Circle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ yucca.py
@@ -0,0 +1,12 @@
+z=0
+y=0
+x=4
+
+while(y<=x):
+    if(y<x):
+        y=y+1
+        z=z+y
+    else:
+        y=y+1
+        z=z-3
+print ("z vaut " + str(z))
