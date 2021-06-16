bb879586b59ccc8a32fe8978eb061acb26c27485
BenoitEP <benoit.epiard@gmail.com>
2021-06-16 10:57:42 +0200

    MAJ 16/06/21

---
+++ Pair-Impair/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pair_Impair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+        private void LB_Result_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int nbSaisie = int.Parse(TB_Saisie.Text);
+                if (nbSaisie % 2 == 0)
+                {
+                    LB_Result.Text = "Le nombre saisis est pair";
+                    TB_Saisie.Text = "";
+                    TB_Saisie.Focus();
+                }
+                else
+                {
+                    LB_Result.Text = "Le nombre saisis est impair";
+                    TB_Saisie.Text = "";
+                    TB_Saisie.Focus();
+                }                
+            }
+            catch
+            {
+                MessageBox.Show("Veuillez taper un nombre entier.");
+                TB_Saisie.Text = "";
+                TB_Saisie.Focus();
+            }            
+        }
+    }
+}
